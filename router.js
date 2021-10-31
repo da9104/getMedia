8d753c8c3a74f64f884b8cb97f9e811366600604
DAMI <dami@DAMIs-MacBook-Pro.local>
2021-10-31 01:44:42 +0000

    modifying HTTP req.

--- router.js
+++ router.js
@@ -17,6 +17,8 @@ router.use(
 
 router.get('/', function(req, res) {
     res.set('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Origin", "https://get--media.herokuapp.com")
     res.header("Access-Control-Allow-Origin", "get--media.herokuapp.com/")
     res.header("Access-Control-Allow-Credentials", "true");
